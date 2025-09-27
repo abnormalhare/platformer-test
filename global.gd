d43106f5792cad69c53372ea182072caac6b5408
CreateSource <abnormalhare822@gmail.com>
2025-09-27 01:38:41 -0400

    mulitplayer fix, last push was multiplayer

--- global.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-var players := {};
-var players_loaded := 0;
--- lobby.gd
+++ lobby.gd
@@ -7,11 +7,16 @@ signal server_disconnected
 const IP_ADDRESS = "127.0.0.1";
 const PORT := 26000;
 
+var players := {};
+var players_loaded := 0;
 var player_info = {"name": "Name"}
 
 func _ready():
-	multiplayer.peer_connected.connect(_on_player_connected);
-	multiplayer.peer_disconnected.connect(_on_player_disconnected);
+	multiplayer.peer_connected.connect(_on_player_connected)
+	multiplayer.peer_disconnected.connect(_on_player_disconnected)
+	multiplayer.connected_to_server.connect(_on_connected_ok)
+	multiplayer.connection_failed.connect(_on_connected_fail)
+	multiplayer.server_disconnected.connect(_on_server_disconnected)
 
 func create_server():
 	var peer = ENetMultiplayerPeer.new();
@@ -21,7 +26,7 @@ func create_server():
 	
 	multiplayer.multiplayer_peer = peer;
 	
-	Global.players[1] = player_info;
+	players[1] = player_info;
 	player_connected.emit(1, player_info)
 
 func create_client():
@@ -33,7 +38,7 @@ func create_client():
 
 func remove_multiplayer_peer():
 	multiplayer.multiplayer_peer = OfflineMultiplayerPeer.new();
-	Global.players.clear();
+	players.clear();
 
 @rpc("call_local", "reliable")
 func load_game():
@@ -42,7 +47,9 @@ func load_game():
 @rpc("any_peer", "call_local", "reliable")
 func player_loaded():
 	if multiplayer.is_server():
-		load_game();
+		players_loaded += 1
+		if players_loaded == 2:
+			load_game();
 
 func _on_player_connected(id):
 	_register_player.rpc_id(id, player_info)
@@ -50,16 +57,16 @@ func _on_player_connected(id):
 @rpc("any_peer", "reliable")
 func _register_player(new_player_info):
 	var new_player_id = multiplayer.get_remote_sender_id();
-	Global.players[new_player_id] = new_player_info;
+	players[new_player_id] = new_player_info;
 	player_connected.emit(new_player_id, new_player_info);
 
 func _on_player_disconnected(id):
-	Global.players.erase(id)
+	players.erase(id)
 	player_disconnected.emit(id)
 
 func _on_connected_ok():
 	var peer_id = multiplayer.get_unique_id()
-	Global.players[peer_id] = player_info
+	players[peer_id] = player_info
 	player_connected.emit(peer_id, player_info)
 
 func _on_connected_fail():
@@ -67,5 +74,5 @@ func _on_connected_fail():
 
 func _on_server_disconnected():
 	remove_multiplayer_peer()
-	Global.players.clear()
+	players.clear()
 	server_disconnected.emit()
